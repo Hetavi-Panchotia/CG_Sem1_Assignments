3e159664d52c4c224e5eee75ccb8ee23dcca9b56
Hetavi Panchotia <hetavipanchotia@gmail.com>
2025-09-15 12:00:47 +0530

    loop assignment

---
+++ C_Assignments/Loops/Assignment 5.c
@@ -0,0 +1,235 @@
+// #include <stdio.h>
+// int main(){
+//     for (int i=1;i<=10;i++){
+//         printf("%d\n",i);
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+//     for (int i=2;i<=20;i++){
+//         if (i%2==0){
+//             printf("%d\n",i);
+//         }
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+//     for (int i=1;i<=20;i++){
+//         if (i%2!=0){
+//             printf("%d\n",i);
+//         }
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+//     for (int i=1;i<=10;i++){
+//         printf("%d\n",i*i);
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+//     int n;
+//     scanf("%d",&n);
+//     for (int i=1;i<=10;i++){
+//         printf("%d\n",i*n);
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+//     for (int i=10;i>=1;i=i-1){
+//         printf("%d\n",i);
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+//     int s=0;
+//     for (int i=1;i<=50;i++){
+//         s=s+i;
+        
+//     }printf("%d\n",s);
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+//     for (int i=1;i<=10;i++){
+//         printf("%d\n",i);
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+//     for (int i=1;i<=50;i++){
+//         if (i%5==0){
+//             printf("%d\n",i);
+//         }
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+//     for (int i=1;i<=5;i++){
+//         printf("%d\n",i*i*i);
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+//     for (int i=1;i<=30;i++){
+//         if (i%3==0){
+//             printf("%d\n",i);
+//         }
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+//     int n,p=1;
+//     scanf("%d",&n);
+//     for (int i=1;i<=n;i++){
+//         p=p*i;
+//     }
+//     printf("%d",p);
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+//     int i=1;
+//     while (i<=10){
+//         printf("%d\n",i);
+//         i++;
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+//     int i=1;
+//     while (i<=20){
+//         if (i%2==0){
+//             printf("%d\n",i);
+//         }
+//         i++;
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+//     int i=1;
+//     while (i<=20){
+//         if (i%2!=0){
+//             printf("%d\n",i);
+//         }
+//         i++;
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+//     int s=0;
+//     int i=1;
+//     while (i<=50){
+//         s=s+i;
+//         i++;
+//     }
+//     printf("%d",s);
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+//     int i=10;
+//     while(i>=1){
+//         printf("%d\n",i);
+//         i=i-1;
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+//     int i=1;
+//     while(i<=70){
+//         if (i%7==0){
+//             printf("%d\n",i);
+//         }
+//         i++;
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+//     int p=1,i=1;
+//     int n;
+//     scanf("%d",&n);
+//     while (i<=n){
+//         p=p*i;
+//         i++;
+//     }
+//     printf("%d",p);
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+//     int i=1;
+//     while (i<=5){
+//         printf("%d\n",i);
+//         i++;
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+//     int i=1;
+//     int n;
+//     scanf("%d",&n);
+//     while (i<=n){
+//         printf("%d\n",i);
+//         i++;
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+//     int i=1;
+//     while(i<=5){
+//         printf("%d\n",i*i);
+//         i++;
+//     }
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+//     int n,i=1;
+//     scanf("%d",&n);
+//     while (n>=i){
+//         printf("%d\n",n);
+//         n=n-1;
+//     }
+//     return 0;
+// }
