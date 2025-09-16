3136100687fa2406d61cadaa0194c0b4d1bbc6bc
Hetavi Panchotia <hetavipanchotia@gmail.com>
2025-09-16 19:00:06 +0530

    more patterns added

---
+++ C_Assignments/Patterns/39.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+
+    int n;
+    scanf("%d",&n);
+
+    for(int i=n+64 ; i>=65 ; i--){
+        for(int j=n+64 ; j>=i; j--){
+            printf(" ");
+        }
+        for (int j=65 ; j<=i ; j++){
+            printf("%c",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
