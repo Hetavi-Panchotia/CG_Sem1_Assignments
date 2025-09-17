e9f8598def5dd2449e5b353ba3139340a5c12d33
Hetavi Panchotia <hetavipanchotia@gmail.com>
2025-09-17 18:29:02 +0530

    more patterns done

---
+++ C_Assignments/Patterns/57.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main (){
+
+    int n;
+    scanf("%d",&n);
+
+    for (int i=1 ; i<=n ; i++){
+        for (int j=1 ; j<=n ; j++){
+            if ( i==1 || i==n || j==1 || j==n){
+                printf("1 ");
+            }
+            else if( i==2 || i==n-1 || j==2 || j==n-1){
+                printf("2 ");
+            }
+            else if (i==3 || i==n-2 || j==3 || j==n-2){
+                printf("3 ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
